4aae027d3ac2bdee405fff3ba4be007e5bd77fcb
Cyrus Team <cyrusteam@cyruslists.com>
2014-02-04 18:36:33 -0500

    download each file to folder named after the episode

--- parsing.rb
+++ parsing.rb
@@ -4,9 +4,14 @@ def sample
   Nokogiri::HTML(File.read 'wzbc_sample.html')
 end
 
+def episode_data
+  episode_rows.slice_before do |row| 
+    !row.css('td.c3[rowspan]').empty?
+  end
+end
+
 def episodes
-  e = episode_rows.slice_before {|row| !row.css('td.c3[rowspan]').empty? }
-  [*e]
+  episode_data.map {|e| Episode.new(e) }
 end
 
 def episode_rows
@@ -17,8 +22,20 @@ def rowspans
   sample.css('td.c3[rowspan]').map {|x| x.attribute('rowspan').value.to_i }  
 end
 
-def test_episodes
-  rowspans == (episodes.map {|e| e.count })
+def test_episode_data
+  raise 'failed' unless rowspans == (episode_data.map {|e| e.count })
+end
+
+def assert_equal(a, b)
+  unless a == b
+    raise "#{a} != #{b}"
+  end
+end
+
+def test_dirname
+  e = Episode.new(nil)
+  def e.title; 'f.o,o/& b-a\'r? (baz)'; end
+  assert_equal(e.dirname, "f_o_o_and_b_a_r___baz_")
 end
 
 class Episode
@@ -34,10 +51,25 @@ class Episode
 
   def title
     b = @episode_elements.map {|e| e.css('b').first }.compact.first
-    b.text if b
+    if b
+      b.text
+    else
+      'unknown'
+    end
+  end
+
+  def dirname
+    title.downcase.gsub(/ |,|\.|-|\'|\/|\?|\(|\)/,'_').gsub('&', 'and')
   end
 
   def dl!
-    
+    Dir.mkdir(dirname) unless Dir.exists? dirname
+    Dir.chdir(dirname) do
+      links.each do |link|
+        puts ?* * 16
+        puts `pwd`
+        puts "wget http://zbconline.com/#{link}"
+      end
+    end
   end
 end
